d3abe1985a159f225e98e493162ad11ab403c0ad
JulioMourajr <juliomourajr@hotmail.com>
2023-05-13 18:55:51 -0300

    Mudanças no component Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
+import styles from "./Home.module.css"
+
 const Home = ()=>{
   return(
     <main>
       <h1>Bem vindo ao nosso site!</h1>
       <p>Esta é a pagina inicial. Fique a vontade para navegar entre as páginas que está na lateral esquerda. Obrigado!</p>
     </main>
-
-    
   )
 }
 

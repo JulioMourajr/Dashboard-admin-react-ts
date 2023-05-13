89e1ae5c9ffe274c1058f9824412bfa6c062a617
JulioMourajr <juliomourajr@hotmail.com>
2023-05-13 16:48:50 -0300

    Mudanças no componente Listagem de experiencias

--- src/pages/curriculo/ManipularInformacoes/InformacoesCard/InformacoesCard.tsx
+++ src/pages/curriculo/ManipularInformacoes/InformacoesCard/InformacoesCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import styles from './InformacoesCard.module.css'
 
 import { Informacoes } from "../../../../services/informacoesService";
@@ -9,14 +10,16 @@ interface InformacoesCardProps{
 
 const InformacoesCard:React.FC<InformacoesCardProps> = ({informacoes})=>{
     const {foto, nome, cargo, resumo} = informacoes
-    return (<div className={styles.card}>
-        <img src={foto} alt={`${nome}'s foto `} className={styles.foto} />
-        <div className={styles.content}>
-            <h3 className={styles.nome}>{nome}</h3>
-            <p className={styles.cargo}>{cargo}</p>
-            <p className={styles.resumo}>{resumo}</p>
+    return (
+    
+        <div className={styles.card}>
+            <img src={foto} alt={`${nome}'s foto `} className={styles.foto} />
+            <div className={styles.content}>
+                <h3 className={styles.nome}>{nome}</h3>
+                <p className={styles.cargo}>{cargo}</p>
+                <p className={styles.resumo}>{resumo}</p>
+            </div>
         </div>
-    </div>
     )
 }
 

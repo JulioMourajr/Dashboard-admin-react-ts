ad7f031dc7c6f9e6813d683c7b35d90284779752
juliomourajr <juliomourajr@hotmail.com>
2023-05-14 02:23:46 -0300

    Refatoração do Componente Home.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./Home.module.css"
 
+import Title from "../../components/common/Title";
+import InfoBox from "../../components/common/InfoBox";
+
+//import {FaGraduationCap, FaBriefcase, FaFolder} from 'react-icons/fa';
+
+import { Projeto, getPortifolio } from "../../services/portifolioService";
+import { Experiencia, getExperienciaById, getExperienciaByTipo } from "../../services/experienciaService";
+
 const Home = ()=>{
+
+  const [experienciasAcademicas, setExperienciasAcademicas] = 
+  useState<Experiencia[]>([]);
+  const [experienciasProfissionais, setExperienciasProfissionais] = 
+  useState<Experiencia[]>([]);
+  const [portifolio, setPortifolio] = 
+  useState<Projeto[]>([]);
+
+  const fetchExperienciasAcademicas = async()=>{
+    try{
+      const response = await getExperienciaByTipo('academico')
+      setExperienciasAcademicas(response)
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  const fetchExperienciasProfissionais = async () =>{
+    try {
+      const response = await getExperienciaByTipo('profissional');
+      setExperienciasProfissionais(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const fetchPortifolio = async () =>{
+    try {
+      const response = await getPortifolio();
+      setPortifolio(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchExperienciasAcademicas();
+    fetchExperienciasProfissionais();
+    fetchPortifolio();
+  }, [])
+
   return(
-    <main>
-      <h1>Bem vindo ao nosso site!</h1>
+    <main className={styles.container}>
+      <Title className={styles.title}>
+        Bem vindo ao nosso site!
+        </Title>
       <p>Esta é a pagina inicial. Fique a vontade para navegar entre as páginas que está na lateral esquerda. Obrigado!</p>
+      <div className={styles.InfoBoxContainer}>
+        <InfoBox
+          title="Experiencias Academicas"
+          value={experienciasAcademicas.length}
+          //icon={<FaGraduationCap size={65}/>}
+          />
+          <InfoBox
+          title="Experiencias Profissionais"
+          value={experienciasProfissionais.length}
+          //icon={<FaBriefcase/>}
+          />
+          <InfoBox
+          title="Projetos no Portifólio"
+          value={portifolio.length}
+          //icon={<FaFolder />}
+          />
+      </div>
     </main>
   )
 }
---
+++ src/services/portifolioService.ts
@@ -0,0 +1,47 @@
+import api from './api'
+
+export interface Projeto{
+    id?: number;
+    link:string;
+    image:string;
+    title:string;
+}
+
+export const createProjeto = async (projeto:Projeto)
+:Promise<Projeto> =>{
+    const response = await api.post<Projeto>("/portifolio", projeto);
+    return response.data;
+}
+
+export const getPortifolio = async():Promise<Projeto[]> => {
+    const response = await api.get<Projeto[]>("./portifolio");
+    return response.data;
+}
+
+export const deleteProjeto = async(id:number | undefined)
+:Promise<Projeto> =>{
+    const response = await api.delete<Projeto>(`/portifolio/
+    ${id}`);
+    return response.data;
+}
+
+export const updateProjeto = async(projeto:Projeto)
+:Promise<Projeto> =>{
+    const response = await api.put<Projeto>(`/portifolio/
+    ${projeto.id}`, projeto);
+    return response.data;
+}
+
+export const getProjeto = async(id:number):Promise<Projeto> => {
+    const response = await api.get<Projeto>(`/portifolio/${id}`);
+    return response.data;
+}
+
+export const createOrUpdateProjeto = async(projeto:Projeto):
+Promise<Projeto> => {
+    if(!projeto.id) {
+        return await createProjeto(projeto);
+    }else{
+        return await updateProjeto(projeto);
+    }
+}
